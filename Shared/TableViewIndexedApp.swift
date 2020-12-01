debaa1623ea73fff53198200e24c53327fdcd4a9
Jan Hovland <jan.hovland@lyse.net>
2020-12-01 18:26:32 +0100

    Initial Commit

---
+++ Shared/TableViewIndexedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewIndexedApp.swift
+//  Shared
+//
+//  Created by Jan Hovland on 01/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct TableViewIndexedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
